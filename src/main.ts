664c9e01e47ddf74749fcbef3e150aaa9c461283
shijianzhong <994129509@qq.com>
2024-09-13 16:56:51 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app').$nextTick(() => {
+  // Use contextBridge
+  window.ipcRenderer.on('main-process-message', (_event, message) => {
+    console.log("main-process-message")
+    console.log(message)
+  })
+})
